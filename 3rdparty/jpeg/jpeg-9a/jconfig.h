b99fe5999e98a74c3e5412b86f6fcdd7bb55a217
chai2010 <chaishushan@gmail.com>
2016-03-30 10:02:23 +0800

    3rdparty/jpeg: add cmake

---
+++ 3rdparty/jpeg/jpeg-9a/jconfig.h
@@ -0,0 +1,9 @@
+// Copyright 2016 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#if defined(_MSC_VER)
+#	include "./jconfig.vc"
+#else
+#	error "TODO"
+#endif
