bc024fc33d7bb514b1665973b25fdf0312f41ca5
chai2010 <chaishushan@gmail.com>
2016-03-30 09:49:38 +0800

    3rdparty/eigen3: add cmake

---
+++ 3rdparty/eigen3/eigen3.cmake
@@ -0,0 +1,9 @@
+# Copyright 2016 <chaishushan{AT}gmail.com>. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+set(EIGEN3_ROOT ${PROJECT_SOURCE_DIR}/3rdparty/eigen3)
+
+include_directories(AFTER
+	${EIGEN3_ROOT}/3rdparty/eigen3
+)
